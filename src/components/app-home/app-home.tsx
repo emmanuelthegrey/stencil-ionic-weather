574f1847aa40160750c51307e48250c425ad020c
emmanuelthegrey <whoisemmanuel@gmail.com>
2019-10-30 21:46:19 -0400

    test and geolocation

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,10 +1,17 @@
 import { Component, h } from '@stencil/core';
+import { Plugins } from "@capacitor/core";
+const { Geolocation } = Plugins;
 
 @Component({
   tag: 'app-home',
   styleUrl: 'app-home.css'
 })
 export class AppHome {
+
+	async componentDidLoad() {
+		let coordinates = await Geolocation.getCurrentPosition();
+		console.log(coordinates);
+	}
 
   render() {
     return [
---
+++ src/components/app-settings/app-settings.spec.ts
@@ -0,0 +1,10 @@
+import { AppSettings } from './app-settings';
+
+describe("app settings initialized", () => {
+	it("builds", () => {
+		expect(new AppSettings()).toBeTruthy();
+	});
+
+	
+
+});
